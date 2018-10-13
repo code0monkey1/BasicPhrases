033ed4f43b39c575f6ed19fd7445861980490db8
Chiranjeev Thomas <chiranjeevcollege@gmail.com>
2018-10-13 17:04:23 +0530

    added comments

--- app/src/main/java/com/example/chiranjeev/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/chiranjeev/basicphrases/MainActivity.java
@@ -6,6 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Step 1 : Set the gridLayout  by using the GridLayout widget and define the number of rows and columns
+ * <p>
+ * Step 2 : next put in a button , and define it's gravity as 1 , and same for the rowWeight =1 and columnWeight=1 ,
+ * by doing this , the button will occupy 1 full unit in the layout ..
+ * <p>
+ * Step 3 : now make a common onClick method that will be the target of all buttons
+ * <p>
+ * Step 4 : in the place of the tags , put the names of the song titles as it is ( without extension )
+ * <p>
+ * Step 5 : now after casting out the button object that was pressed ,
+ * extract it's name using getResources().getIdentifier and then put it as a parameter put in the name of the
+ * song after extracting it .
+ * the 2nd parameter "raw" is the name of the folder
+ * and the 3rd param get's the packageName()
+ */
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
 
@@ -15,47 +31,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Step 3 :
     public void englishToFrench(View view) {
+        // Step 5 :
+
         Button button = (Button) view;
 
-        String tag = button.getTag().toString();
-
-        setButtonSound(tag);
+        mediaPlayer = MediaPlayer.create(this,
+                getResources().getIdentifier(button.getTag().toString(),
+                        "raw",
+                        getPackageName()));
 
         startSound();
     }
 
     private void startSound() {
         mediaPlayer.start();
-    }
-
-    private void setButtonSound(String tag) {
-        switch (tag) {
-            case "dyse":
-                mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
-                break;
-            case "ge":
-                mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
-                break;
-            case "h":
-                mediaPlayer = MediaPlayer.create(this, R.raw.hello);
-                break;
-            case "hay":
-                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
-                break;
-            case "ili":
-                mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
-                break;
-            case "mni":
-                mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
-                break;
-            case "p":
-                mediaPlayer = MediaPlayer.create(this, R.raw.please);
-                break;
-            default:
-                mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
-
-        }
     }
 
 
